948a3f6119978ae92fa091eb28dcf5e8d293176f
kprose07 <khaylahrscholar@gmail.com>
2025-06-20 00:19:20 -0400

    fixed bundle error need to install native wind

--- beta-verse/babel.config.js
+++ beta-verse/babel.config.js
@@ -1,7 +1,17 @@
 module.exports = function (api) {
   api.cache(true);
   return {
-    presets: [["babel-preset-expo", { jsxImportSource: "nativewind" }]],
-    plugins: ["nativewind/babel"],
+    presets: [["babel-preset-expo"]],
+    plugins: [
+      [
+        "module-resolver",
+        {
+          alias: {
+            "@": "./",
+          },
+          extensions: [".js", ".jsx", ".ts", ".tsx"],
+        },
+      ],
+    ],
   };
 };
